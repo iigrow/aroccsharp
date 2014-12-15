dd89ec6cabd7e9cccb58aa7b83192f3279ceb31f
xuejpGitHub <xuejpmail@gmail.com>
2014-12-15 22:35:28 +0800

    Add the files about thread

---
+++ src/Aroc/ArocThread.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Runtime.Remoting.Messaging;
+using System.Security.Permissions;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Aroc
+{
+    /// <summary>
+    /// About thread
+    /// </summary>
+    public class ArocThread
+    {
+        /// <summary>
+        /// 获取当前线程的物理线程ID
+        /// </summary>
+        /// <returns></returns>
+        [DllImport("kernel32.dll")]
+        public static extern int GetCurrentThreadId();
+
+        /// <summary>
+        /// 返回物理线程的线程
+        /// </summary>
+        public static ProcessThread CurrentProcessThread
+        {
+            get
+            {
+                int threadId = GetCurrentThreadId();
+                return (from ProcessThread pt in Process.GetCurrentProcess().Threads
+                        where pt.Id == threadId
+                        select pt).Single();
+            }
+        }
+
+        /// <summary>
+        /// 创建一个新线程
+        /// </summary>
+        /// <param name="action"></param>
+        /// <returns></returns>
+        public static Thread CreateThread(Action action)
+        {
+            return new Thread(new ThreadStart(action));
+        }
+
+        /// <summary>
+        /// 创建一个新线程
+        /// </summary>
+        /// <param name="action"></param>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public static Thread CreateThread(Action<object> action)
+        {
+            return new Thread(new ParameterizedThreadStart(action));
+        }
+
+        /// <summary>
+        /// P/Invoke 使用OS的物理线程来执行
+        /// <param name="action">函数</param>
+        /// </summary>
+        [SecurityPermission(SecurityAction.Demand, Flags = SecurityPermissionFlag.ControlThread)]
+        public static Thread PInvoke(Action action)
+        {
+            return new Thread(() =>
+            {
+                Thread.BeginThreadAffinity();
+                action();
+                Thread.EndThreadAffinity();
+            });
+        }
+
+        /// <summary>
+        ///  P/Invoke 使用OS的物理线程来执行
+        /// </summary>
+        /// <param name="action">函数</param>
+        /// <param name="processAffinity">指定线程运行在哪个CPU核心上(4核为1,2,4,8)</param>
+        /// <returns></returns>
+        [SecurityPermission(SecurityAction.Demand, Flags = SecurityPermissionFlag.ControlThread)]
+        public static Thread PInvoke(Action action, int processAffinity)
+        {
+            return new Thread(() =>
+            {
+                Thread.BeginThreadAffinity();
+                CurrentProcessThread.ProcessorAffinity = new IntPtr(processAffinity);
+                action();
+                Thread.EndThreadAffinity();
+            });
+        }
+
+        /// <summary>
+        /// 在线程池执行线程
+        /// </summary>
+        /// <param name="action"></param>
+        /// <param name="state"></param>
+        /// <returns></returns>
+        public static bool ExecuteAsync(Action<object> action, object state = null)
+        {
+            return ThreadPool.QueueUserWorkItem(new WaitCallback(action), state);
+        }
+
+        /// <summary>
+        /// 设置逻辑调用上下文数据
+        /// </summary>
+        public static void SetCallContext(string name, object data)
+        {
+            CallContext.LogicalSetData(name, data);
+        }
+
+        /// <summary>
+        /// 获取逻辑调用上下文数据
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        public static object GetCallContext(string name)
+        {
+            return CallContext.LogicalGetData(name);
+        }
+
+        /// <summary>
+        /// 在线程池创建可取消的线程
+        /// </summary>
+        /// <param name="actions"></param>
+        /// <returns>线程协作取消对象</returns>
+        public static CancellationTokenSource CreateThreads(params Action<CancellationToken>[] actions)
+        {
+            CancellationTokenSource cts = new CancellationTokenSource();
+            foreach (var action in actions)
+            {
+                ThreadPool.QueueUserWorkItem((obj) => { action(cts.Token); });
+            }
+            return cts;
+        }
+
+        /// <summary>
+        /// 注册在取消CancellationTokenSource时调用的方法
+        /// </summary>
+        /// <param name="ct"></param>
+        /// <param name="isSync">顺序执行/回调会被send(执行send方法，要阻塞调用线程直到到目标线程处理后才会返回，)给已捕捉的SynchronizationContext对象，由该对象决定调用哪个action</param>
+        /// <param name="actions"></param>
+        public static void ReigsterCancelCallback(CancellationToken ct, bool isSync, params Action<object>[] actions)
+        {
+            foreach (var action in actions)
+            {
+                ct.Register(action, isSync);
+            }
+        }
+    }
+}
+
---
+++ test/ArocTest/ArocThreadTest.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Aroc;
+using System.Threading;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace ArocTest
+{
+    [TestClass]
+    public class ArocThreadTest
+    {
+        [TestMethod]
+        public void GetCurrentThreadIdTest()
+        {
+            int threadId = ArocThread.GetCurrentThreadId();
+            Assert.AreNotEqual(0, threadId);
+            Assert.AreNotEqual(threadId, Thread.CurrentThread.ManagedThreadId);
+        }
+
+        [TestMethod]
+        public void CurrentProcessThreadTest()
+        {
+            Assert.AreEqual(ArocThread.GetCurrentThreadId(), ArocThread.CurrentProcessThread.Id);
+        }
+
+        [TestMethod]
+        public void PInvokeTest()
+        {
+            Thread thread = ArocThread.PInvoke(() =>
+            {
+
+            });
+
+            thread.Start();
+            Thread.Sleep(100);
+            thread.Abort();
+
+            int processAffinity = 2;
+            int processAffinity2 = -1;
+            thread = ArocThread.PInvoke(() =>
+            {
+                processAffinity2 = Process.GetCurrentProcess().ProcessorAffinity.ToInt32();
+            }, processAffinity);
+
+            thread.Start();
+            Thread.Sleep(100);
+            //Assert.AreEqual(processAffinity, processAffinity2);
+            thread.Abort();
+        }
+
+        [TestMethod]
+        public void CreateThreadTest()
+        {
+            Thread thread = ArocThread.CreateThread(() =>
+            {
+                for (int i = 0; i < 100; i++)
+                { }
+            });
+            Assert.IsNotNull(thread);
+
+            thread = ArocThread.CreateThread(obj =>
+            {
+                for (int i = 0; i < 100; i++)
+                { }
+            });
+
+            Assert.IsNotNull(thread);
+        }
+
+        [TestMethod]
+        public void ExecuteAsyncTest()
+        {
+            bool result = ArocThread.ExecuteAsync(obj =>
+            {
+                for (int i = 0; i < 100; i++)
+                {
+                }
+            });
+
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void CallContextTest()
+        {
+            string tagData = "hello world!";
+            string actualyData = "";
+
+            ArocThread.SetCallContext("test", tagData);
+
+            // 阻止执行上下文流动
+            ExecutionContext.SuppressFlow();
+            ArocThread.ExecuteAsync(obj =>
+            {
+                try
+                {
+                    actualyData = Convert.ToString(ArocThread.GetCallContext("test"));
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            });
+            Thread.Sleep(1000);
+            ExecutionContext.RestoreFlow();
+
+            Assert.AreNotEqual(actualyData, tagData);
+
+            ArocThread.ExecuteAsync(obj =>
+            {
+                actualyData = Convert.ToString(ArocThread.GetCallContext("test"));
+            });
+
+            Thread.Sleep(1000);
+
+            Assert.AreEqual(actualyData, tagData);
+        }
+
+        public void CatchException()
+        {
+            try
+            {
+                ThreadPool.QueueUserWorkItem((obj) =>
+                {
+                    throw new Exception("error");
+                });
+            }
+            catch (AggregateException aex)
+            {
+                // AggregateException重写了exception的GetBaseException方法，实现会返回作为问题根源的最内层的异常
+                aex.GetBaseException();
+
+                // 创建一个新的AggregateException其InnerExceptions属性包含一个异常列表，其中的异常是通过遍历原始AggregateException的内层异常层次结构生成的。
+                aex.Flatten();
+
+                // 为AggregateException包含的每个异常都调用该回调方法 该回调方法返回true来表示已经处理 返回false表示未处理
+                // 如果有一个异常没有处理就创建一个新的AggregateException对象，其中只包含未处理的异常，并抛出该异常。
+                aex.Handle((ex) => { Console.WriteLine("OK"); return true; });
+            }
+        }
+
+        public void CancelTask()
+        {
+            CancellationTokenSource cts = new CancellationTokenSource();
+            Task t = new Task(() =>
+            {
+                while (true)
+                {
+                    // 如果调用了Cancel函数则 会抛出OperationCanceledException异常
+                    cts.Token.ThrowIfCancellationRequested();
+                }
+            });
+
+            t.Start();
+
+            try
+            {
+                Thread.Sleep(100);
+
+                t.Wait();
+
+                cts.Cancel();
+            }
+            catch (AggregateException aex)
+            {
+                // 忽略取消操作造成的异常
+                aex.Handle(ex => ex is OperationCanceledException);
+            }
+        }
+
+        public void ContinueTask()
+        {
+            Task t = new Task(() =>
+            {
+                new Task(() => { Console.WriteLine("first"); }).Start();
+                new Task(() => { Console.WriteLine("second"); }).Start();
+            });
+
+            // t.ContinueWith(task =>Array.ForEach(),TaskContinuationOptions.AttachedToParent);
+        }
+    }
+}
